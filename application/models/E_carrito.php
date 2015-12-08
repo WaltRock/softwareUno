f723ef861415357c8374ef079b2b33b3c52f101c
Princess Reyes Campos <prc_lu96@hotmail.com>
2015-12-08 13:00:36 -0500

    Princces subida 2

---
+++ application/models/E_carrito.php
@@ -0,0 +1,10 @@
+<?php
+
+class E_carrito extends CI_Model {
+    
+    public function getProducto() {
+        //$query = $this->db->query("CALL nombredelprcoedimientoalmacenadoquellamas");
+        //return $query->result_array();
+        return "holaPrincess";
+    }
+}
